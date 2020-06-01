0a3db80155b82c6e29ef1454bc35fa202e6193ed
Ryan Moulton <ryanmoulton116@gmail.com>
2020-06-01 14:37:33 -0700

    add mobile validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
 import styles from '../styles/Form.module.css';
+import icon from '../images/icon-error.svg';
 
 const Form = () => {
   const [formInput, setFormInput] = useState({
     firstName: '',
+    firstNameError: '',
+    firstNameStyles: {
+      icon: 'none',
+      border: 'lightgray',
+    },
     lastName: '',
+    lastNameError: '',
+    lastNameStyles: {
+      icon: 'none',
+      border: 'lightgray',
+    },
     email: '',
+    emailError: '',
+    emailStyles: {
+      icon: 'none',
+      border: 'lightgray',
+    },
     password: '',
+    passwordError: '',
+    passwordStyles: {
+      icon: 'none',
+      border: 'lightgray',
+    },
   });
 
   const handleChange = (e) => {
@@ -15,7 +36,7 @@ const Form = () => {
     if (name === 'firstName') {
       setFormInput((prevFormInput) => {
         return {
-          prevFormInput,
+          ...prevFormInput,
           firstName: value,
         };
       });
@@ -24,7 +45,7 @@ const Form = () => {
     if (name === 'lastName') {
       setFormInput((prevFormInput) => {
         return {
-          prevFormInput,
+          ...prevFormInput,
           lastName: value,
         };
       });
@@ -33,7 +54,7 @@ const Form = () => {
     if (name === 'email') {
       setFormInput((prevFormInput) => {
         return {
-          prevFormInput,
+          ...prevFormInput,
           email: value,
         };
       });
@@ -42,8 +63,103 @@ const Form = () => {
     if (name === 'password') {
       setFormInput((prevFormInput) => {
         return {
-          prevFormInput,
+          ...prevFormInput,
           password: value,
+        };
+      });
+    }
+  };
+
+  const validateForm = () => {
+    setFormInput((prevFormInput) => {
+      return {
+        ...prevFormInput,
+        firstNameError: '',
+        firstNameStyles: {
+          icon: 'none',
+          border: 'lightgray',
+        },
+        lastNameError: '',
+        lastNameStyles: {
+          icon: 'none',
+          border: 'lightgray',
+        },
+        emailError: '',
+        emailStyles: {
+          icon: 'none',
+          border: 'lightgray',
+        },
+        passwordError: '',
+        passwordStyles: {
+          icon: 'none',
+          border: 'lightgray',
+        },
+      };
+    });
+
+    if (formInput.firstName === '') {
+      setFormInput((prevFormInput) => {
+        return {
+          ...prevFormInput,
+          firstNameError: 'First Name cannot be empty',
+          firstNameStyles: {
+            icon: 'block',
+            borderColor: 'hsl(0, 100%, 74%) ',
+          },
+        };
+      });
+    }
+
+    if (formInput.lastName === '') {
+      setFormInput((prevFormInput) => {
+        return {
+          ...prevFormInput,
+          lastNameError: 'Last Name cannot be empty',
+          lastNameStyles: {
+            icon: 'block',
+            borderColor: 'hsl(0, 100%, 74%) ',
+          },
+        };
+      });
+    }
+
+    if (formInput.email === '') {
+      setFormInput((prevFormInput) => {
+        return {
+          ...prevFormInput,
+          emailError: 'Email cannot be empty',
+          emailStyles: {
+            icon: 'block',
+            borderColor: 'hsl(0, 100%, 74%) ',
+          },
+        };
+      });
+    }
+
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (formInput.email !== '' && !re.test(formInput.email)) {
+      setFormInput((prevFormInput) => {
+        return {
+          ...prevFormInput,
+          emailError: 'Please enter a valid email',
+          emailStyles: {
+            icon: 'block',
+            borderColor: 'hsl(0, 100%, 74%) ',
+          },
+        };
+      });
+    }
+
+    if (formInput.password === '') {
+      setFormInput((prevFormInput) => {
+        return {
+          ...prevFormInput,
+          passwordError: 'Password cannot be empty',
+          passwordStyles: {
+            icon: 'block',
+            borderColor: 'hsl(0, 100%, 74%) ',
+          },
         };
       });
     }
@@ -51,8 +167,7 @@ const Form = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submitted!');
-    console.log(formInput);
+    validateForm();
   };
 
   return (
@@ -65,38 +180,86 @@ const Form = () => {
         </div>
       </div>
       <div className={styles.form}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-          value={formInput.firstName}
-          onChange={handleChange}
-        />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          value={formInput.lastName}
-          onChange={handleChange}
-        />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Email Address"
-          name="email"
-          value={formInput.email}
-          onChange={handleChange}
-        />
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Password"
-          name="password"
-          value={formInput.password}
-          onChange={handleChange}
-        />
+        <div className={styles.inputWrapper}>
+          <input
+            className={styles.input}
+            id="firstName"
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            value={formInput.firstName}
+            onChange={handleChange}
+            style={{ borderColor: formInput.firstNameStyles.borderColor }}
+          />
+          <img
+            className={styles.errorIcon}
+            src={icon}
+            alt="error icon"
+            style={{ display: formInput.firstNameStyles.icon }}
+          />
+          <em className={styles.errorMsg}>{formInput.firstNameError}</em>
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <input
+            className={styles.input}
+            id="lastName"
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            value={formInput.lastName}
+            onChange={handleChange}
+            style={{ borderColor: formInput.lastNameStyles.borderColor }}
+          />
+          <img
+            className={styles.errorIcon}
+            src={icon}
+            alt="error icon"
+            style={{ display: formInput.lastNameStyles.icon }}
+          />
+          <em className={styles.errorMsg}>{formInput.lastNameError}</em>
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <input
+            className={styles.input}
+            id="email"
+            type="text"
+            placeholder="Email Address"
+            name="email"
+            value={formInput.email}
+            onChange={handleChange}
+            style={{ borderColor: formInput.emailStyles.borderColor }}
+          />
+          <img
+            className={styles.errorIcon}
+            src={icon}
+            alt="error icon"
+            style={{ display: formInput.emailStyles.icon }}
+          />
+          <em className={styles.errorMsg}>{formInput.emailError}</em>
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <input
+            className={styles.input}
+            id="password"
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formInput.password}
+            onChange={handleChange}
+            style={{ borderColor: formInput.passwordStyles.borderColor }}
+          />
+          <img
+            className={styles.errorIcon}
+            src={icon}
+            alt="error icon"
+            style={{ display: formInput.passwordStyles.icon }}
+          />
+          <em className={styles.errorMsg}>{formInput.passwordError}</em>
+        </div>
+
         <input
           className={classNames(styles.input, styles.submitBtn)}
           type="submit"
